b154a0a44403c40b46e1af7d08c8e1683bb556aa
Nelson <nelson.salazar@digitalhouse.com>
2022-10-13 10:45:51 -0300

    update

---
+++ 16va-clase/prueba-native/App.js
@@ -0,0 +1,11 @@
+import { Text, View} from 'react-native';
+import Hola from './src/components/Hola/Hola';
+import Home from './src/screens/Home/Home';
+import Clase from './src/screens/ClaseForms/Clase';
+export default function App() {
+  return (
+    <>
+      <Clase />
+    </>
+  );
+}
---
+++ 16va-clase/prueba-native/src/screens/ClaseFlatList/Clase.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import ProductList from '../../components/ProductList/ProductList'
+
+export default function Clase() {
+  return (
+    <View>
+      <ProductList />
+    </View>
+  )
+}
